cb62b536db0e0e737617026f547f6bc911e79172
Sammy H. jr <132605016+SammyHJr@users.noreply.github.com>
2026-07-29 16:53:24 +0200

    Implemented the Button and JSX function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useRef } from "react";
-import logo from './logo.svg';
+import { useRef, useState } from "react";
 import './App.css';
 
 
 function App() {
   const fileInputRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleButtonClick = () => {
     fileInputRef.current.click();
@@ -13,20 +13,60 @@ function App() {
   const handleFileChange = (event) => {
     const file = event.target.files[0];
 
-    if(file) {
+    if (file) {
       console.log("Selected file:", file);
       console.log("File name:", file.name);
       console.log("File Size:", file.size);
-      console.log("File type:", file.type)
+      console.log("File type:", file.type);
     }
   };
 
+  const handleUpload = () => {
+    if (!selectedFile) {
+      console.log("No file selected")
+      return;
+    }
+    console.log("Selected file:", selectedFile);
+    console.log("File name:", selectedFile.name);
+    console.log("File Size:", selectedFile.size);
+    console.log("File type:", selectedFile.type);
+
+  };
+
+
+
   return (
     <div className="App">
-        <header className = "App-header">
-          <h1>AI Document Analyzer</h1>
-          ##ADD the rest here tomorrow
-        </header>
+      <header className="App-header">
+        <h1>AI Document Analyzer</h1>
+        {/* Hidden file input */}
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          accept=".pdf"
+          style={{ display: "none" }}
+        />
+
+        {/* choose button */}
+        <button className="Button" onClick={handleButtonClick}> Choose File </button>
+
+        {/* Upload button */}
+        {selectedFile && (
+          <div>
+            <p>Selected file: {selectedFile.name}</p>
+
+            <button
+              className="Button"
+              onClick={handleUpload}
+            >
+              Upload Document
+            </button>
+          </div>
+        )}
+
+
+      </header>
     </div>
   );
 }
